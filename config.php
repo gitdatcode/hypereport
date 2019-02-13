22ca518c1eac6d874c569f462740929c94c61930
Ann Daramola <ann.daramola@gmail.com>
2019-02-12 20:15:33 -0800

    Sets up January 2019 template

--- config.php
+++ config.php
@@ -2,4 +2,7 @@
 
 return [
     'basePath' => '/var/www/hypereport.ann',
+    'baseUrl' => '//hypereport.ann/',
+    'assetUrl' => '//hypereport.ann/assets',
+    'template' => '/var/www/hypereport.ann/templates',
 ];
