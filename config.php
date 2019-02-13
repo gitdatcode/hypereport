a1136e8ca298f983a1f3ae0264523de8a0d97622
Ann Daramola <ann.daramola@gmail.com>
2019-02-12 20:40:44 -0800

    Implements hype archive

--- config.php
+++ config.php
@@ -4,5 +4,5 @@ return [
     'basePath' => '/var/www/hypereport.ann',
     'baseUrl' => '//hypereport.ann/',
     'assetUrl' => '//hypereport.ann/assets',
-    'template' => '/var/www/hypereport.ann/templates',
+    'templatePath' => '/var/www/hypereport.ann/templates',
 ];
