0a978e332fd72028aeb9660e6ebe9c8983d6e895
Ann Daramola <ann.daramola@gmail.com>
2019-02-15 00:51:48 -0800

    Updates January 2019 template

--- templates/january.php
+++ templates/january.php
@@ -4,43 +4,51 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sliding Triple View Layout</title>
+    <title>DATCODE Hype Report: January 2019</title>
     <meta name="description" content="Sliding Triple View Layout with Visible Adjoining Sections" />
     <meta name="keywords" content="visible sides, layout, sliding, three panels, css transforms, web development, tutorial, template" />
     <meta name="author" content="Codrops" />
-    <link rel="shortcut icon" href="<?php asset('/january/../favicon.ico'); ?>">
-    <link rel="stylesheet" type="text/css" href="<?php asset('/january/css/normalize.css'); ?>" />
-    <link rel="stylesheet" type="text/css" href="<?php asset('/january/css/demo.css'); ?>" />
-    <link rel="stylesheet" type="text/css" href="<?php asset('/january/css/component.css'); ?>" />
+    <link rel="shortcut icon" href="<?php asset('favicon.ico'); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php url('css/bootstrap.min.css'); ?>" />
+    <link href="https://fonts.googleapis.com/css?family=Abril+Fatface|Open+Sans:300,400,400i" rel="stylesheet"> 
+    <link rel="stylesheet" type="text/css" href="<?php asset('/january/css/january.css'); ?>" />
     <script src="<?php asset('/january/js/modernizr.custom.js'); ?>"></script>
-
 </head>
 <body>
-<div id="vs-container" class="vs-container" style="background-image: url(<?php asset('/january/images/1.jpg'); ?>);">
-    <header class="vs-header">
-        <h1>DATCODE HypeReport <span>January 2019</span></h1>
-        <ul class="vs-nav">
-            <?php foreach($reports as $report): ?>
-            <li data-id="<?php echo $report['id']; ?>"><a href="#section-<?php echo $index+1; ?>">#0<?php echo $report['id']; ?></a></li>
-            <?php endforeach; ?>
-        </ul>
-    </header>
-    <div class="vs-wrapper">
+<div class="wrapper">
+    <section class="reports">
+        <section class="report summary">
+            <article>
+                <h1>DATCODE</h1>
+                <h2>Hype Report<br />January 2019</h2>
+            </article>
+        </section>
         <?php foreach($reports as $report): ?>
 
-        <section id="section-<?php echo $report['id']; ?>">
-            <div class="vs-content">
-                <h2><?php echo $report['firstname']; ?></h2>
-                <div class="report">
-                    <p><?php echo $report['report']; ?></p>
-                </div>
-            </div>
+        <section 
+            id="report_<?php echo $report['id']; ?>"
+            class="report"
+            style="background-image:url(<?php asset("/images/january/{$report['id']}.jpg"); ?>);">
+            <img src="<?php asset("/january/images/{$report['id']}.jpg"); ?>" alt="">
+            <article>
+                <h2 class="report__title">
+                    <?php echo $report['firstname'] . ' ' . mb_substr($report['lastname'], 0, 1, 'utf-8'); ?>.
+                </h2>
+                <p class="report__text"><?php echo $report['report']; ?></p>
+                <a
+                    class="report__hype"
+                    target="_blank"
+                    href="https://twitter.com/home?status=Check%20out%20<?php echo $report['firstname']; ?>'s%20January%202019%20Hype%20Report!%3A%20https%3A//hype.report%3Ajanuary%23report_<?php echo $report['id']; ?>" role="button">
+                    Hype This
+                </a>
+            </article>
         </section>
+
         <?php endforeach; ?>
-    </div>
-</div><!-- /vs-container -->
-<script src="<?php asset('/january/js/classie.js'); ?>"></script>
-<script src="<?php asset('/january/js/hammer.min.js'); ?>"></script>
-<script src="<?php asset('/january/js/main.js'); ?>"></script>
+        <section class="report conclusion">
+            <p class="report__text">Come join us at <a href="http://www.datcode.io/#sign-up">datcode.io</a>!</p>
+        </section>
+    </section>
+</div>
 </body>
 </html>
