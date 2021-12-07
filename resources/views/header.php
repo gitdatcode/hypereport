f958839deac9ee7a968c37957dbf60d091288f9c
ann daramola <ann.daramola@gmail.com>
2021-12-07 10:59:30 -0800

    Fix styles

--- resources/views/header.php
+++ resources/views/header.php
@@ -1,11 +1,16 @@
 <header id='nav' class="site-header">
-	  <a id="logo" href="/" title="">
+    <a class="main-cta" href="/submit">
+        <?php if (str_contains(\Illuminate\Support\Facades\Request::url(), 'submit')) {
+        echo 'HypeReport 2021';
+        } else { echo 'Submit'; }?>
+    </a>
+	  <a id="logo" class="site-logo" href="/" title="">
 	    <img alt="DATCODE" src="/theme/year_month/assets/images/DATCODE_ENERGY_LOGOMARK_WHITE.png" />
 	  </a>
-	  <nav id="site-nav" class="site-nav">
-	  	<a href="https://www.datcode.io">Go to datcode.io</a>
-	    <a href="https://www.datcode.io/resources">Resources</a>
-				<a class="nav-sign-up" href="<?php echo url('/submit'); ?>">Submit</a>
+    <nav id="site-nav" class="site-nav">
+	  	<a href="https://hype.report">Hype Reports</a>
+        <a target="_blank" href="https://www.datcode.io">DATCODE</a>
+        <a class="nav-sign-up" href="<?php echo url('/submit'); ?>">Submit</a>
 	  </nav>
 	  <nav id="site-nav-mobile" class="site-nav-mobile">
 	  	<a id="menu-icon" href="javascript:void(0);" class="icon" onclick="toggleMenu()">
@@ -13,8 +18,8 @@
 	      </a>
 	  	<div id="site-nav-links" class="site-nav-links not-visible">
 	  		<ul>
-			    <li><a href="https://www.datcode.io">Go to datcode.io</a></li>
-			    <li><a href="https://www.datcode.io/resources">Resources</a></li>
+                <li><a href="https://hype.report">Hype Reports</a></li>
+			    <li><a target="_blank" href="https://www.datcode.io">DATCODE</a></li>
 						<li><a class="nav-sign-up" href="/submit">SUBMIT</a></li>
 				</ul>
 			</div>
